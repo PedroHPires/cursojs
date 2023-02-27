4dddb15a7f34d0c4dd229e85f5a139d168befff1
PedroHPires <pedrohpires.dev@gmail.com>
2023-02-27 10:21:01 -0300

    aula011

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel>60){ // Condição simples
+    console.log('Você estava acima da velovidade permitida. MULTADO!')
+}
+console.log("Dirija sempre usando cinto de segurança")
