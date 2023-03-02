1c518af2373176f80b196b966ecedbb19d2b410d
PedroHPires <pedrohpires.dev@gmail.com>
2023-03-02 09:43:54 -0300

    ex015

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -1,12 +1,1 @@
-let sexo 
-masculino(){
-    sexo = 'Homem'
-}
-feminino(){
-    sexo = 'Mulher'
-}
-
-verificar();{
-    var idade = Number(document.getElementById('data')) - 2023
-    if()
-}
+alert('Olá')
