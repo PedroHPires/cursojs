35460143e19e59c155112885fbc291dcae713481
PedroHPires <pedrohpires.dev@gmail.com>
2023-03-04 00:27:53 -0300

    aula 13

---
+++ aula013/node_3ac0766a9dc98.tmp.js
@@ -0,0 +1,18 @@
+var c = 1
+while(c < 6){
+    console.log('Tudo bem?')
+}
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
