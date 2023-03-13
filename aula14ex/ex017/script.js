065880afde306f32272900caac99411adc4b0582
PedroHPires <pedrohpires.dev@gmail.com>
2023-03-13 20:00:30 -0300

    ex 17 (listas <select>)

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function gerarTabuada(){
+    num = document.getElementById('num')
+    res = document.getElementById('res')
+    if (num.value.length == 0){
+        alert('Por favor, insira um número!')
+    }else{
+        res.innerHTML = ''
+        let n = Number(num.value)
+        for (let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            res.appendChild(item)
+        }
+    }
+    
+   
+
+}
